ce5d468e6f745af5900213f5f9ffc8783f0c36a9
Sunny Tran <tran.sunny217@gmail.com>
2019-12-08 01:22:50 -0500

    begun change to MCMC

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 import pyro
 import pyro.distributions as dist
 import torch.distributions.constraints as constraints
-from pyro.infer import SVI, Trace_ELBO
+from pyro.infer import SVI, Trace_ELBO, MCMC, HMC, EmpiricalMarginal
 from pyro.optim import Adam
 
 from random import shuffle
@@ -13,26 +13,13 @@ from random import shuffle
 # sample as many thetas as in observtions
 # compare if the bags of observations and thetas match up
 
+'''
 
-true_alpha = 10 # uniform from -alpha to alpha
-coherence = 0.2
-samples = 100
-data = []
-
-coherent_dist = dist.Normal(true_alpha, 5) # constant * 1 / coherence
-for i in range(int(coherence * samples)):
-    data.append(pyro.sample("coherent_point", coherent_dist))
-
-incoherent_dist = dist.Uniform(-90, 90)
-for i in range(samples - int(coherence * samples)):
-    data.append(pyro.sample("incoherent_point", incoherent_dist))
-
-shuffle(data)
 
 
 def model(data):
     hypothesis_prob = torch.tensor(0.5)
-    alpha = torch.tensor(10.0) # should not be learned
+    
 
     h = pyro.sample("h", dist.Bernoulli(hypothesis_prob))
 
@@ -66,15 +53,71 @@ optimizer = Adam(adam_params)
 
 # setup the inference algorithm
 svi = SVI(model, guide, optimizer, loss=Trace_ELBO())
+'''
 
-n_steps = 50
-# do gradient steps
-for _ in range(n_steps):
-    svi.step(data)
+## Alpha ################
+alpha = torch.tensor(10.0) # should not be learned
+
+true_alpha_sampler = dist.Uniform(-alpha, alpha) # uniform from -alpha to alpha, holds universe of possible values?
+true_alpha = true_alpha_sampler.sample()
+print('true_alpha', true_alpha) # this is where boundary line lies?
+dim = 1
+#########################
+
+## Data #################
+coherence = 0.2
+samples = 100
+data = []
+
+coherent_dist = dist.Normal(true_alpha, 3) # constant * 1 / coherence
+for i in range(int(coherence * samples)):
+    data.append(pyro.sample("coherent_point", coherent_dist))
+
+incoherent_dist = dist.Uniform(-alpha, alpha) # incoherence chosen from all possible vectors
+for i in range(samples - int(coherence * samples)):
+    data.append(pyro.sample("incoherent_point", incoherent_dist))
+
+shuffle(data)
+data = torch.tensor(data)
+print('data shape', data.shape)
+#########################
+
+
+## Model ################
+def model(data):
+    coefs_mean = torch.zeros(dim) # (-alpha + alpha) / 2
+    coefs = pyro.sample('beta', dist.Normal(coefs_mean, torch.tensor([alpha] * dim)))
+    y = pyro.sample('y', dist.Bernoulli(logits=(coefs * data).sum(-1))) #obs=labels
+    return y
+    # hypothesis_prob = torch.tensor(0.5)
+    # h = pyro.sample("h", dist.Bernoulli(hypothesis_prob))
+    # print('h', h)
+    # if h == 1:
+    #     theta = pyro.sample("theta", dist.Uniform(0, alpha))
+    # else:
+    #     theta = pyro.sample("theta", dist.Uniform(-alpha, 0))
+
+    # # loop over the observed data
+    # for i in range(len(data)):
+    #     pyro.sample(f"obs_{i}", theta, obs=data[i]) # convert to MCMC, create sample function for one sample, run n times (right now it's one theta)
+
+
+hmc_kernel = HMC(model, step_size=0.1, num_steps=50)
+mcmc = MCMC(hmc_kernel, num_samples=100, warmup_steps=100)
+mcmc.run(data)
+print(mcmc.get_samples()['beta'], mcmc.get_samples()['beta'].shape)
+
+# mcmc.get_samples()['beta'].mean(0)  # doctest: +SKIP
+
+
+# n_steps = 50
+# # do gradient steps
+# for _ in range(n_steps):
+#     svi.step(data)
 
 
 # grab the learned variational parameters
-hypothesis_prob_q = pyro.param("hypothesis_prob_q").item()
-alpha_q = pyro.param("alpha_q").item()
+# hypothesis_prob_q = pyro.param("hypothesis_prob_q").item()
+# alpha_q = pyro.param("alpha_q").item()
 
-print(hypothesis_prob_q, alpha_q)
+# print(hypothesis_prob_q, alpha_q)
